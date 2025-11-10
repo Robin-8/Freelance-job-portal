f9ae3acacf9eea6b4b2bd50c65b98dce992a5575
Robin Shaji <robinshaji888@gmail.com>
2025-11-10 06:01:34 +0530

    vite and tailwind installed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+      <>
+      <div className='text-2xl font-bold bg-blue-500'>
+          <h1>Hai this from robin</h1>
+      </div>
+      </>
+  )
+}
+
+export default App
