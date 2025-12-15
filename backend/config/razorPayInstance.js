c066f02230d806781e32655f0be8a193db39d919
Robin Shaji <robinshaji888@gmail.com>
2025-12-15 19:21:02 +0530

    razorpay updated

--- backend/config/razorPayInstance.js
+++ backend/config/razorPayInstance.js
@@ -1,8 +1,12 @@
 import Razorpay from "razorpay";
 
+if (!process.env.RAZORPAY_KEY_ID || !process.env.RAZORPAY_KEY_SECRET) {
+  throw new Error("Razorpay keys missing in env");
+}
+
 const instance = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_id: process.env.RAZORPAY_KEY_ID.trim(),
+  key_secret: process.env.RAZORPAY_KEY_SECRET.trim(),
 });
 
 export default instance;
