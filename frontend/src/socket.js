460898341d4ea96f0bf62ead304a73060d239b99
Robin Shaji <robinshaji888@gmail.com>
2025-12-15 16:26:41 +0530

    socket io updated

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
   transports: ["websocket"],
+  withCredentials: true,
 });
 
 export default socket;
